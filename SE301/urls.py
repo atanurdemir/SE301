a9a0c9025a0eafa948e175a28a9b2c77d0357b07
Atanur <atanurdemir@gmail.com>
2019-11-09 23:09:20 +0300

    pages

--- SE301/urls.py
+++ SE301/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from pages.views import home_view
+from Pages.views import home_view
 urlpatterns = [
     path('', home_view, name='home'),
     path('admin/', admin.site.urls),
