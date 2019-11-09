f45134ceafd01813ac06138c737b6936be2725ec
Taha Demirer <tahademirer@hotmail.com>
2019-11-09 17:50:52 +0300

    update2

--- SE301/settings.py
+++ SE301/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Admin.apps.AdminConfig',
     'Doctor.apps.DoctorConfig',
+    'Admin.apps.AdminConfig',
+
 ]
 
 MIDDLEWARE = [
