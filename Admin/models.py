6cb3e8f7e1efe326ec1b7ed770305a1b92a79b42
Taha Demirer <tahademirer@hotmail.com>
2019-11-09 17:43:01 +0300

    admin model created

--- Admin/models.py
+++ Admin/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Admin(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    userId = models.CharField(max_length=11, unique=True)
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=50)
+    GSM = models.CharField(max_length=10, unique=True)
+    Address = models.CharField(max_length=200)
+
