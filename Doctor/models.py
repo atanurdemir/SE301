37fc0fa80594379c36a5f55ec475415e0e3032d6
Atanur <atanurdemir@gmail.com>
2019-11-09 16:58:08 +0300

    doctorModel

--- Doctor/models.py
+++ Doctor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Doctor(models.Model):
+    first_Name = models.CharField(max_length=30)
+    last_Name = models.CharField(max_length=30)
+    userId = models.IntegerField()
+    password = models.CharField(max_length=20)
+    title = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    gsm = models.IntegerField()
+    address = models.CharField(max_length=200)
+    workDays = models.DateTimeField(max_length=30)
+    workHours = models.DateTimeField(max_length=30)
