9fa1ade9a42e249cfbfbd4dd5ffb9b1f18031f7c
Atanur <atanurdemir@gmail.com>
2019-11-09 17:32:38 +0300

    doctorApp

--- Doctor/models.py
+++ Doctor/models.py
@@ -3,11 +3,19 @@ from django.db import models
 class Doctor(models.Model):
     first_Name = models.CharField(max_length=30)
     last_Name = models.CharField(max_length=30)
-    userId = models.IntegerField()
+    userId = models.CharField(max_length=11, unique=True)
     password = models.CharField(max_length=20)
-    title = models.CharField(max_length=30)
+    TITLE = (
+        ('INT', 'Intern'),
+        ('PRA', 'Practitioner'),
+        ('SPE','Specialist'),
+        ('OPE','Operating Surgeon'),
+        ('APR','Assistant Professor'),
+        ('PRO', 'Professor'),
+    )
+    title = models.CharField(max_length=1, choices=TITLE)
     email = models.CharField(max_length=30)
-    gsm = models.IntegerField()
+    gsm = models.CharField(max_length=10, unique=True)
     address = models.CharField(max_length=200)
-    workDays = models.DateTimeField(max_length=30)
-    workHours = models.DateTimeField(max_length=30)
+    workDays = models.DateField(max_length=30)
+    workHours = models.TimeField(max_length=30)
