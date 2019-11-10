fb8a8de689aa8255b622f58c3d5be1d9f6412be1
alpince <44799963+alpince@users.noreply.github.com>
2019-11-10 19:40:13 +0300

    Patient Kodları

--- Patient/models.py
+++ Patient/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    first_Name = models.CharField(max_length=30)
+    last_Name = models.CharField(max_length=30)
+    gender = models.CharField(max_length=10)
+    userId = models.CharField(max_length=11, unique=True)
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=30)
+    gsm = models.CharField(max_length=10, unique=True)
+    address = models.CharField(max_length=200)
+
