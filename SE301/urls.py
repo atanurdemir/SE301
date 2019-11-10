4ce1137b7f4a05985d4137b66b6034eed6914796
Atanur <atanurdemir@gmail.com>
2019-11-10 03:37:31 +0300

    template

--- SE301/urls.py
+++ SE301/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path
-from Pages.views import home_view, register_view, admin_view, doctor_view, patient_view
+from Pages.views import home_view, register_view, admin_view, doctor_view, patient_view, contact_view, forget_view, \
+    login_view, news_view
 
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', home_view, name='home'),
+    path('contact/', contact_view),
+    path('forgetPassword/', forget_view),
+    path('login/', login_view),
+    path('news/', news_view),
     path('register/', register_view),
     path('adminPage/', admin_view),
     path('doctorPage/', doctor_view),
