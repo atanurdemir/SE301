ac7e6a7b0d18946734c97a4c8f454654ec0d2a4e
Atanur <atanurdemir@gmail.com>
2019-11-17 23:47:23 +0300

    u42

--- Pages/views.py
+++ Pages/views.py
@@ -2,37 +2,47 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def home_view(request,*args, **kwargs):
-    print(args,kwargs)
+def home_view(request, *args, **kwargs):
+    print(args, kwargs)
     print(request.user)
     return render(request, "home.html", {})
 
-def admin_view(request,*args, **kwargs):
-    print(args,kwargs)
+
+def admin_view(request, *args, **kwargs):
+    print(args, kwargs)
     print(request.user)
     return render(request, "adminPage.html", {})
 
-def doctor_view(request,*args, **kwargs):
-    print(args,kwargs)
+
+def doctor_view(request):
+    return render(request, 'doctorPage.html', {'doctors': doctor_view()})
+
+
+def doctor_view(request, *args, **kwargs):
+    print(args, kwargs)
     print(request.user)
     return render(request, "doctorPage.html", {})
 
-def patient_view(request,*args, **kwargs):
-    print(args,kwargs)
+
+def patient_view(request, *args, **kwargs):
+    print(args, kwargs)
     print(request.user)
     return render(request, "patientPage.html", {})
 
-def contact_view(request,*args, **kwargs):
-    print(args,kwargs)
+
+def contact_view(request, *args, **kwargs):
+    print(args, kwargs)
     print(request.user)
     return render(request, "contact.html", {})
 
-def forget_view(request,*args, **kwargs):
-    print(args,kwargs)
+
+def forget_view(request, *args, **kwargs):
+    print(args, kwargs)
     print(request.user)
     return render(request, "forgetPassword.html", {})
 
-def news_view(request,*args, **kwargs):
-    print(args,kwargs)
+
+def news_view(request, *args, **kwargs):
+    print(args, kwargs)
     print(request.user)
     return render(request, "news.html", {})
