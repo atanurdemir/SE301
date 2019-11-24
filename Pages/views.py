007c8dbe7f6b74840902d8830b39923f453d7df1
atanurdemir <atanurdemir@gmail.com>
2019-11-25 01:26:33 +0300

    doctorPage

--- Pages/views.py
+++ Pages/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required()
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
