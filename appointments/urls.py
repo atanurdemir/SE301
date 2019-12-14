38c0bd372403f71d12a1fb8831e8596e6338d529
atanurdemir <atanurdemir@gmail.com>
2019-12-15 00:20:00 +0300

    searchDoctor function
    searchDoctor function made by Atanur Demir.

--- appointments/urls.py
+++ appointments/urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls import url
 from . import views
 from django.urls import path
-from .views import list_of_appointments, list_of_patients, add_appointment
+from .views import list_of_appointments, list_of_patients, add_appointment, list_of_doctors
+
 app_name = 'appointments'
 
-urlpatterns=[
+urlpatterns = [
     # url(r'^$', views.appointment_list, name="list"),
     # url(r'^(?P<DoctorName>[\w-]+)/$', views.appointment_details, name="detail"),
     # path('appointment_list', list_of_appointments.as_view(), name="appointment_list")
     url(r'^appointment_list/$', list_of_appointments.as_view(), name='list1'),
     url(r'^patient_list/$', list_of_patients.as_view(), name='list2'),
+    url(r'^doctors_list/$', list_of_doctors.as_view(), name='list3'),
     url(r'add-appointment/$', views.add_appointment, name='add_appointment'),
 ]
